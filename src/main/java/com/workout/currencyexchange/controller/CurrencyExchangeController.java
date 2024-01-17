b2a873b60a14b12ce3340317bd115f68ba7df680
Shunmugabharathi <shunmugabharathi@gmail.com>
2024-01-17 17:12:48 -0600

    added update comment line

--- src/main/java/com/workout/currencyexchange/controller/CurrencyExchangeController.java
+++ src/main/java/com/workout/currencyexchange/controller/CurrencyExchangeController.java
@@ -35,7 +35,7 @@ public class CurrencyExchangeController
         return new ResponseEntity<>(saved, HttpStatus.CREATED);
     }
 
-
+//update call
     @PutMapping ("/updateRate")
     public ResponseEntity<ExchRateInfo> updateCurrencyExchange(){
 
