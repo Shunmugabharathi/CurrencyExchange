3d08c3da788c38c4bf6ed90666ee2442588cf7e2
Shunmugabharathi <shunmugabharathi@gmail.com>
2024-01-17 17:44:55 -0600

    comment added in repo class

--- src/main/java/com/workout/currencyexchange/respository/ExchangeRateRepo.java
+++ src/main/java/com/workout/currencyexchange/respository/ExchangeRateRepo.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 public interface ExchangeRateRepo extends JpaRepository<ExchRateInfo, Long> {
 
+    //add findby method
     ExchRateInfo findByFromCurrencyAndToCurrency(String fromCurrency, String toCurrency);
 
 }
